0c2c4bf39867898cefc50a48b8cf1dca13d6ff9f
B16041612冯峰 <951115439@qq.com>
2018-05-25 23:40:22 +0800

    第一次提交
    完成了页面的基础内容，部分注册页面

---
+++ myBlog/js/background.js
@@ -0,0 +1,10 @@
+$.backstretch([
+		'http://api.asilu.com/cdn/img/bg/444.jpg',
+		'http://api.asilu.com/cdn/img/bg/445.jpg',
+		'http://api.asilu.com/cdn/img/bg/446.jpg',
+		'http://api.asilu.com/cdn/img/bg/447.jpg',
+		'http://api.asilu.com/cdn/img/bg/448.jpg'
+	], {
+		fade : 1000, // 动画时长
+		duration : 2000 // 切换延时
+});
